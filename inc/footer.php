baabff9bd9bb3b6a50176ddd84247bc666980892
Renato Diniz <redinizf@hotmail.com>
2018-06-14 09:19:38 -0300

    separação de header e footer

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
