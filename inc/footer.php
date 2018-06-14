ca601ea4973666c69922245127ce581c646a1403
Renato Diniz <redinizf@hotmail.com>
2018-06-14 17:51:09 -0300

    ajustes no banco e visual

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,16 @@
 
 </div> <!-- fecha div conteúdo-->
 <!-- Footer -->
-<footer class="py-5 bg-dark">
+<footer class="py-5 bg-azul">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Exame 2018</p>
     </div>
     <!-- /.container -->
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="<?php echo BASEURL; ?>node_modules/popper.js/dist/popper.min.js"></script>
 <script src="<?php echo BASEURL; ?>node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo BASEURL; ?>node_modules/popper.js/dist/umd/popper.min.js"></script>
 <script src="<?php echo BASEURL; ?>node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
--- index.php
+++ index.php
@@ -1,82 +1,80 @@
 <?php 
 require_once 'config.php'; 
 require_once DBAPI;
-?>	
+?>
 
 <?php include(HEADER_TEMPLATE); ?>
 
-  <!-- Page Content -->
-  <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-    <!-- Jumbotron Header -->
-    <header class="jumbotron my-4">
-      <h1 class="display-3">A Warm Welcome!</h1>
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt
-        odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-      <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
-    </header>
-
-    <!-- Page Features -->
-    <div class="row text-center">
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
+  <!-- Jumbotron Header -->
+  <header class="jumbotron my-4">
+    <div class="row">
+      <div class="col-8">
+        <h1 class="display-3">Seja bem-vindo!</h1>
+        <p class="lead">O
+          <b>Exame</b> é um site que permite aos professores criarem um banco de questões e utilizá-las para montar provas e
+          aplicá-las aos seus alunos de maneira fácil e rápida.</p>
+        <a href="#" class="btn btn-primary btn-lg">Comece agora mesmo!</a>
       </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa
-              natus architecto.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
+      <div class="col-4" align="center">
+        <img src="<?php echo BASEURL; ?>img/livro.svg">
       </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa
-              natus architecto.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-
     </div>
-    <!-- /.row -->
+  </header>
+
+  <!-- Page Features -->
+  <div class="row text-center">
+
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card">
+        <div class="card-imagem m-3 align-content-center" align="center">
+          <img src="<?php echo BASEURL; ?>img/201603.svg">
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">Cadastre questões</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Monte sua prova</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa
+            natus architecto.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Responda as provas do seu professor</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="card-body">
+          <h4 class="card-title">Veja os resultados</h4>
+          <p class="card-text">O professor pode avaliar as provas dos seus alunos e os alunos podem verificar o seu desempenho nas provas, visualizando
+            as questões corretas e se preparando melhor para os próximos testes.</p>
+        </div>
+      </div>
+    </div>
 
   </div>
-  <!-- /.container -->
+  <!-- /.row -->
 
-  <?php include(FOOTER_TEMPLATE); ?>
+</div>
+<!-- /.container -->
+
+<?php include(FOOTER_TEMPLATE); ?>
